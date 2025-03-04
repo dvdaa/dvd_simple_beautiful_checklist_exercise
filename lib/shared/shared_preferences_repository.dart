6b7e42816205e8459295e14f4fe83b4b4e97f6c1
dvdaa <dvd.aa@web.de>
2025-03-04 13:16:27 +0100

    Lösung

--- lib/shared/shared_preferences_repository.dart
+++ lib/shared/shared_preferences_repository.dart
@@ -3,53 +3,51 @@ import 'package:simple_beautiful_checklist_exercise/shared/database_repository.d
 
 class SharedPreferencesRepository implements DatabaseRepository {
   static const String _key = "tasks";
-  late final SharedPreferences prefs;
+  late final SharedPreferences _prefs;
+  List<String> _items = [];
 
   SharedPreferencesRepository() {
     init();
   }
 
   Future<void> init() async {
-    prefs = await SharedPreferences.getInstance();
+    _prefs = await SharedPreferences.getInstance();
+    _items = _prefs.getStringList(_key) ?? [];
   }
 
   @override
   Future<int> get itemCount async {
-    int itemCount = 0;
-    if (prefs.getStringList(_key) != null) {
-      itemCount = prefs.getStringList(_key)!.length;
-    }
-    return itemCount;
+    return _items.length;
   }
 
   @override
   Future<List<String>> getItems() async {
-    return prefs.getStringList(_key) ?? [];
+    return _items;
   }
 
   @override
   Future<void> addItem(String item) async {
-    List<String> tasks = prefs.getStringList(_key) ?? [];
-    tasks.add(item);
-    await prefs.setStringList(_key, tasks);
+    if (item.isNotEmpty && !_items.contains(item)) {
+        _items.add(item);
+        await _prefs.setStringList(_key, _items);
+    }
   }
 
   @override
   Future<void> deleteItem(int index) async {
-    List<String> tasks = prefs.getStringList(_key) ?? [];
-    if (tasks.length > index) {
-      tasks.removeAt(index);
+    if (_items.length > index) {
+      _items.removeAt(index);
     }
-    await prefs.setStringList(_key, tasks);
+    await _prefs.setStringList(_key, _items);
   }
 
   @override
   Future<void> editItem(int index, String newItem) async {
-    List<String> tasks = prefs.getStringList(_key) ?? [];
-
-    if (tasks.length > index) {
-      tasks[index] = newItem;
+    if (newItem.isNotEmpty) {
+      if (!_items.contains(newItem) && _items.length > index) {
+        _items[index] = newItem;
+      }
+      await _prefs.setStringList(_key, _items);
     }
-    await prefs.setStringList(_key, tasks);
   }
 }
